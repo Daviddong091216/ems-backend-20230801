67fe9c6dc2e764e0f85f8b3826eff99b097fbc68
daviddong091216 <daviddong201712@gmail.com>
2023-08-15 19:36:00 -0500

    20230815

--- src/main/java/com/example/ems/entity/Employee.java
+++ src/main/java/com/example/ems/entity/Employee.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -27,4 +26,36 @@ public class Employee {
 
     @Column(name="email_id", nullable = false, unique = true)
     private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/com/example/ems/mapper/EmployeeMapper.java
+++ src/main/java/com/example/ems/mapper/EmployeeMapper.java
@@ -5,11 +5,19 @@ import com.example.ems.entity.Employee;
 
 public class EmployeeMapper {
     public static EmployeeDto mapToEmployeeDto(Employee employee){
-        return new EmployeeDto(
+        return new Employee(
           employee.getId(),
           employee.getFirstName(),
           employee.getLastName(),
-          employee.getEmail()      
+          employee.getEmail()
+        );
+    }
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
         );
     }
 }
